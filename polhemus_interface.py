71a522e3512bb5ed00dd218059a5f3dae42e5ea1
MelecaZane <23663079@student.uwa.edu.au>
2024-08-19 11:22:42 +0800

    Rename function to be more concise

--- polhemus_interface.py
+++ polhemus_interface.py
@@ -3,7 +3,7 @@ import polhemus
 '''
 Takes in an 'int' amount of polhemus trackers to initialise and returns a list of all the tracker objects.
 '''
-def initialise_all_trackers(amount: int) -> list:
+def initialise_trackers(amount: int) -> list:
     tracker_list = [None]*amount
     for i in range(amount):
         tracker_list[i] = polhemus.polhemus()
