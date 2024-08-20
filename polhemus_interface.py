3ee315b0f69a66d5127834e813e6493aead48893
MelecaZane <23663079@student.uwa.edu.au>
2024-08-20 10:58:50 +0800

    Cleaned up naming and added ability to disable stylus

--- polhemus_interface.py
+++ polhemus_interface.py
@@ -2,7 +2,7 @@ import polhemus
 import time
 
 
-def initialise_trackers(amount: int) -> list:
+def initialise_polhemus(amount: int) -> list:
     """
     Takes in an 'int' amount of polhemus trackers to initialise and returns a list of all the tracker objects.
     """
@@ -12,7 +12,7 @@ def initialise_trackers(amount: int) -> list:
         tracker_list[i].Initialize()
     return tracker_list
 
-def get_tracker_data(tracker_list: list) -> list[dict]:
+def get_polhemus_data(tracker_list: list, stylus: bool) -> list[dict]:
     """
     Takes in list of tracker objects, reads the data from each tracker, and returns a list of dictionaries containing
     the data for each tracker. 
@@ -22,24 +22,41 @@ def get_tracker_data(tracker_list: list) -> list[dict]:
     for i in range(tracker_amount):
         tracker_list[i].Run() # This method seems to not initialise the tracker, but rather read in the data from it.
         timestamp = time.time()
-        data[i] = {
-            "Timestamp": timestamp,
-            "PositionTooltipX1": tracker_list[i].PositionTooltipX1,
-            "PositionTooltipY1": tracker_list[i].PositionTooltipY1,
-            "PositionTooltipZ1": tracker_list[i].PositionTooltipZ1,
-            "AngleX1": tracker_list[i].AngleX1,
-            "AngleY1": tracker_list[i].AngleY1,
-            "AngleZ1": tracker_list[i].AngleZ1,
-            "PositionTooltipX2": tracker_list[i].PositionTooltipX2,
-            "PositionTooltipY2": tracker_list[i].PositionTooltipY2,
-            "PositionTooltipZ2": tracker_list[i].PositionTooltipZ2,
-            "AngleX2": tracker_list[i].AngleX2,
-            "AngleY2": tracker_list[i].AngleY2,
-            "AngleZ2": tracker_list[i].AngleZ2,
-            "StylusButton": tracker_list[i].StylusButton,
-            "Sensor1": tracker_list[i].Sensor1,
-            "Sensor2": tracker_list[i].Sensor2
-        }
+        if stylus:
+            data[i] = {
+                "Timestamp": timestamp,
+                "PositionX1": tracker_list[i].PositionTooltipX1,
+                "PositionY1": tracker_list[i].PositionTooltipY1,
+                "PositionZ1": tracker_list[i].PositionTooltipZ1,
+                "AngleX1": tracker_list[i].AngleX1,
+                "AngleY1": tracker_list[i].AngleY1,
+                "AngleZ1": tracker_list[i].AngleZ1,
+                "PositionX2": tracker_list[i].PositionTooltipX2,
+                "PositionY2": tracker_list[i].PositionTooltipY2,
+                "PositionZ2": tracker_list[i].PositionTooltipZ2,
+                "AngleX2": tracker_list[i].AngleX2,
+                "AngleY2": tracker_list[i].AngleY2,
+                "AngleZ2": tracker_list[i].AngleZ2,
+                "StylusButton": tracker_list[i].StylusButton,
+                "Sensor1": tracker_list[i].Sensor1,
+                "Sensor2": tracker_list[i].Sensor2
+            }
+        else:
+            data[i] = {
+                "Timestamp": timestamp,
+                "PositionX1": tracker_list[i].PositionTooltipX1,
+                "PositionY1": tracker_list[i].PositionTooltipY1,
+                "PositionZ1": tracker_list[i].PositionTooltipZ1,
+                "AngleX1": tracker_list[i].AngleX1,
+                "AngleY1": tracker_list[i].AngleY1,
+                "AngleZ1": tracker_list[i].AngleZ1,
+                "PositionX2": tracker_list[i].PositionTooltipX2,
+                "PositionY2": tracker_list[i].PositionTooltipY2,
+                "PositionZ2": tracker_list[i].PositionTooltipZ2,
+                "AngleX2": tracker_list[i].AngleX2,
+                "AngleY2": tracker_list[i].AngleY2,
+                "AngleZ2": tracker_list[i].AngleZ2
+            }
     return data
 
 def close_trackers(tracker_list: list):
@@ -51,10 +68,10 @@ def close_trackers(tracker_list: list):
 
 
 if __name__ == "__main__":
-    trackers = initialise_trackers(1)
+    trackers = initialise_polhemus(1)
     with open("test_output.csv", "w") as file:
         while True:
-            data = get_tracker_data(trackers)
+            data = get_polhemus_data(trackers, False)
             print(data)
             file.write(str(data) + "\n")
             time.sleep(0.1)
