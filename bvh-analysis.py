7173919e7aaf51716cbc9493999e4c7af62a71b2
AliAlt1 <99878772+AliAlt1@users.noreply.github.com>
2024-08-27 21:10:58 +0800

    Add code to print joint names in BVH file

--- bvh-analysis.py
+++ bvh-analysis.py
@@ -7,3 +7,4 @@ with open("BVH-Recording3.bvh") as f:
 # Extract and print basic information
 print(f"Number of frames: {bvh_data.nframes}")
 print(f"Frame time: {bvh_data.frame_time}")
+print("Joint names:", *bvh_data.get_joints(), sep="\n")
