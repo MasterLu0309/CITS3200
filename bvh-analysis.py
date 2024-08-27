ae5427dc2e82cd00a6c5f49f0fa1177213efbada
AliAlt1 <99878772+AliAlt1@users.noreply.github.com>
2024-08-27 21:09:15 +0800

    Add code to print number of frames in BVH file

--- bvh-analysis.py
+++ bvh-analysis.py
@@ -3,3 +3,6 @@ from bvh import Bvh
 # Read the BVH file and parse it
 with open("BVH-Recording3.bvh") as f:
     bvh_data = Bvh(f.read())
+
+# Extract and print basic information
+print(f"Number of frames: {bvh_data.nframes}")
