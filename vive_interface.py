bd232d83728bf1913dfb537b317ff83c2731c6ed
AliAlt1 <99878772+AliAlt1@users.noreply.github.com>
2024-08-29 12:40:47 +0800

    Iterate through devices and check if they are trackers

--- vive_interface.py
+++ vive_interface.py
@@ -5,3 +5,9 @@ steamvr.init()
 
 # Get the number of tracked devices
 num_devices = steamvr.get_device_count()
+
+# Iterate through devices and check if they are trackers
+for i in range(num_devices):
+  device_class = steamvr.get_device_class(i)
+    if device_class == steanvr.DeviceClass.GenericTracker:
+      print("Tracker on", i, "found")
