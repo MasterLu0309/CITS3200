35323558872d80405a685d33049ac8c0d46477ed
AliAlt1 <99878772+AliAlt1@users.noreply.github.com>
2024-08-29 12:05:37 +0800

    Extract position from pose matrix

--- vive_interface.py
+++ vive_interface.py
@@ -6,3 +6,9 @@ v.print_discovered_objects()
 if len(v.devices["tracker"]) == 0:
     print("No trackers found!")
     exit(1)
+
+while True:
+    for device_key in v.devices["tracker"]:
+        tracker = v.devices["tracker"][device_key]
+        print(f"Tracker {device_key}:")
+        print(f"  Position: {tracker.get_pose_matrix()[:3, 3]}")
