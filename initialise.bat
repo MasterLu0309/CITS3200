2de54a5637ed3faf97711d71452176f025403301
MelecaZane <23663079@student.uwa.edu.au>
2024-09-10 14:49:29 +0800

    Updated initialisation file to create a completely portable python install for use. Eliminates need for admin access.

--- initialise.bat
+++ initialise.bat
@@ -1,10 +1,14 @@
 cd /d %~dp0
-python -m venv venv
+curl -L -O https://github.com/winpython/winpython/releases/download/7.1.20240203final/Winpython64-3.11.8.0.exe
+echo Installing Winpython version 3.11.8 (this can take a while!)
+start /b /wait Winpython64-3.11.8.0.exe -y
+%~dp0\WPy64-31180\python-3.11.8.amd64\python.exe -m venv venv
 call venv\Scripts\activate
+del Winpython64-3.11.8.0.exe
 curl -L -O https://github.com/ultraleap/leapc-python-bindings/archive/refs/heads/main.zip
 tar -xf main.zip
 del main.zip
-python -m pip install --upgrade pip
+%~dp0\WPy64-31180\python-3.11.8.amd64\python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 ren leapc-python-bindings-main leapc
 python -m build .\leapc\leapc-cffi
