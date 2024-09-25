215164679b371122341f17bf90af8499b6190128
Jay Kim <131582258+wwwwvwwwwvvwwwvw@users.noreply.github.com>
2024-09-26 00:05:46 +0800

    Update vive_interface.py

--- vive_interface.py
+++ vive_interface.py
@@ -12,7 +12,7 @@ num_devices = steamvr.get_device_count()
 # Iterate through devices and check if they are trackers
 for i in range(num_devices):
   device_class = steamvr.get_device_class(i)
-    if device_class == steanvr.DeviceClass.GenericTracker:
+    if device_class == steamvr.DeviceClass.GenericTracker:
       # Get the device's pose (position and orientation)
       pose = steamvr.get_device_pose(i)
       position = pose.m[12:15] # Extract position (x, y, z)
